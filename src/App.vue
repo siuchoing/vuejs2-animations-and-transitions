d28d2687f55f6c520fcec5342e947bb950ba7ff2
altantic <15200582@life.hlkbu.edu.hk>
2020-01-22 22:48:11 +0800

    create fade animation

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
                 <hr>
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br><br>
-                <transition>
+                <!-- Only one element for transition-->
+                <transition name="fade">
                     <div class="alert alert-info" v-if="show">This is some Info</div>
                 </transition>
             </div>
@@ -24,6 +25,32 @@
     }
 </script>
 
+/********************************************
+* .fade-enter: is attached for one frame at the beginning
+* .fade-enter-active: is attached for the whole, at each element in animation time
+* .fade where default opacity = 1
+********************************************/
 <style>
+/* ---------- With named transition ---------- */
+    /* transparent when fade */
+    .fade-enter {
+        opacity: 0;
+    }
 
+    /* non-transparent duration */
+    .fade-enter-active {
+        transition: opacity 1s;
+        /*opacity = 1 (default)*/
+    }
+
+    /*non-transparent*/
+    .fade-leave {
+        /*opacity = 1 (default)*/
+    }
+
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+    }
+/* ---------- End of with named transition ---------- */
 </style>
